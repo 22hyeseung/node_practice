ee1367a4edc2a9adf7f5f4071c15fcc655955f75
huusz <irinso93@gmail.com>
2017-09-02 12:24:08 +0900

    ejs 추가

--- router/main.js
+++ router/main.js
@@ -1,8 +1,11 @@
-module.exports = function(app, fs) {
+module.exports = function(app, fs)
+{
   app.get('/', function(req, res){
-    res.render('index.html')
+    res.render('index', {
+      title: "My HomePage",
+      length: 5
+    })
   });
-  app.get('/about', function(req, res){
-    res.render('about.html');
-  })
 }
+// JSON 데이터를 render 메소드의 두번째 인자로 전달하면
+// 페이지에서 데이터를 사용할 수 있다.
