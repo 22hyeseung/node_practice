a926d8f4253389cafa0ceec4a9255ee1f46eee4b
huusz <irinso93@gmail.com>
2017-09-02 11:47:24 +0900

    오타...!

--- router/main.js
+++ router/main.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   app.get('/', function(req, res){
-    res.render(index.html)
+    res.render('index.html')
   });
   app.get('/about', function(req, res){
     res.render('about.html');
