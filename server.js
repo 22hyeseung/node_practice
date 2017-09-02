925bdad37f7e588d5ef9d13a393ad6a5bb9feecb
huusz <irinso93@gmail.com>
2017-09-02 11:46:23 +0900

    라우터 모듈, view 추가

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 // 1. express 서버 생성
 var express = require('express');
 var app = express();
+// 라우터 모듈인 main.js를 불러와서 app에 전달해준다.
+var router = require('./router/main')(app);
+
+// 서버가 읽을 수 있도록 HTML의 위치를 정의해준다.
+app.set('views', __dirname + '/views');
+
+// 서버가 HTML 렌더링을 할 때, EJS 엔진을 사용하도록 설정한다.
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 var server = app.listen(3000, function(){
   console.log("Express server has started on port 3000")
 });
 
-// 여기까지만 작성하면 cannot GET / 이라는 에러가 나타난다.
-// Router를 정의하지 않았기 때문이다.
 
-// 2. Router로 Request 처리하기
-// get
-app.get('/', function(req, res){
-  res.send('Hello World');
-})
